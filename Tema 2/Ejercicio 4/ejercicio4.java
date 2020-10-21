f2454997a5ed7f75c61df4c08d3bc67f0357faaf
githubDaw1 <72400915+githubDaw1@users.noreply.github.com>
2020-10-21 16:37:44 +0200

    Update ejercicio4.java

--- Tema 2/Ejercicio 4/ejercicio4.java
+++ Tema 2/Ejercicio 4/ejercicio4.java
@@ -3,14 +3,14 @@ public class ejercicio4 {
   
     double cantidadeneuros;
 
-    System.out.print("Introducir una cantidad en euros: ");
-    cantidadeneuros = Integer.parseInt (System.console().readLine());
+    System.out.print ("Introducir una cantidad en euros: ");
+    cantidadEnEuros = Integer.parseInt (System.console().readLine());
   
     double total;
-    total = (cantidadeneuros * 166.386);
+    total = (cantidadEnEuros * 166.386);
     
     System.out.println ();
-    System.out.println ("Euros introducidos: " + cantidadeneuros);
+    System.out.println ("Euros introducidos: " + cantidadEnEuros);
     System.out.println ("Pesetas: " + total);
   }
 }
