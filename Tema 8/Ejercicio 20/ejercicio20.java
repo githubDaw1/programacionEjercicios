1c97589b5a7f982abc334b21b5f5faccbceccbf9
githubDaw1 <72400915+githubDaw1@users.noreply.github.com>
2021-01-13 17:23:06 +0100

    Add files via upload

--- Tema 8/Ejercicio 20/ejercicio20.java
+++ Tema 8/Ejercicio 20/ejercicio20.java
@@ -3,25 +3,61 @@ import java.util.Scanner;
 public class ejercicio20 {
   public static void main (String[] args) {
 
-    int n;
+    int numeroMinimo;
+    int numeroMaximo;
     int minimo = 0;
     int maximo = 10;
 
-    Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner(System.in);
 
-    System.out.print ("Cuantos numeros quieres introducir: ");
-    n = scanner.nextInt ();
-
-    devuelveNumerosAleatorios (n, minimo, maximo);
-  }
-
-  public static void devuelveNumerosAleatorios (int n, int minimo, int maximo) {
+    System.out.print ("Cuantos nueros quieres introducir: ");
+    int n = scanner.nextInt ();
 
     int[] numerosIntroducidos = new int [n];
 
     for (int i = 0; i < n; i++) {
       numerosIntroducidos[i] = (int)(Math.random () * maximo);
-      System.out.print(numerosIntroducidos[i] + " | ");
+      System.out.print (numerosIntroducidos[i] + " | ");
     }
+
+    System.out.println ();
+
+    for (int i = 0; i < n; i++) {
+
+      if (i == 0) {
+        minimo = numerosIntroducidos[i];
+        maximo = numerosIntroducidos[i];
+      }
+
+      else if (i > 0) {
+
+        numeroMinimo = devuelveNumeroMinimo (numerosIntroducidos, minimo, i);
+        numeroMaximo = devuelveNumeroMaximo (numerosIntroducidos, maximo, i);
+
+        if (i == n - 1) {
+          System.out.println ();
+          System.out.println ("Maximo: " + numeroMaximo);
+          System.out.println ("Minimo: " + numeroMinimo);
+        }
+      }
+    }
+  }
+
+  public static int devuelveNumeroMinimo (int[] numerosIntroducidos, int minimo, int i) {
+
+    if (numerosIntroducidos[i] < minimo) {
+      minimo = numerosIntroducidos[i];
+    }
+
+    return minimo;
+  }
+
+  public static int devuelveNumeroMaximo (int[] numerosIntroducidos, int maximo, int i) {
+
+    if (numerosIntroducidos[i] > maximo) {
+      maximo = numerosIntroducidos[i];
+    }
+
+    return maximo;
   }
 }
